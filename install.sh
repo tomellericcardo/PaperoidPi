918246e375473ac9d893533e9ab6ffdeddc42c0b
Riccardo Tomelleri <tomelleri.riccardo@gmail.com>
2020-11-27 17:56:53 +0100

    Change main script behaviour and update installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 apt update
-apt install -y python3-pip libbluetooth-dev libhidapi-dev libatlas-base-dev python3-llvmlite python-llvmlite llvm-dev libopenjp2-7-dev libtiff5 python3-picamera
+apt install -y python3-pip python3-gpiozero python3-picamera python3-llvmlite libbluetooth-dev libhidapi-dev libatlas-base-dev python-llvmlite llvm-dev libopenjp2-7-dev libtiff5
 pip3 install --no-cache-dir -r requirements.txt
-echo You need to reboot your Pi before using this
+echo You need to reboot your Pi before using this tool
--- start.py
+++ start.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from gpiozero import Button
+from os import system
+
 from printer import Printer
 from picamera import PiCamera
 
 
-camera = PiCamera()
 printer = Printer('FC:58:FA:32:69:2D')
-camera.capture('pictures/test.jpg')
-printer.print('pictures/test.jpg')
+
+camera = PiCamera()
+camera.rotation = 90
+
+b = Button(12)
+
+
+while True:
+    b.wait_for_press()
+    if b.held_time() < 3:
+        camera.capture('pictures/test.jpg')
+        printer.print('pictures/test.jpg')
+    else:
+        system('shutdown now')
+        break
