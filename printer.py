00300cdf3e809c1ef75b9ab83ca5f1ac684d10c6
Riccardo Tomelleri <tomelleri.riccardo@gmail.com>
2020-11-27 14:50:23 +0100

    Should work now

--- printer.py
+++ printer.py
@@ -6,4 +6,4 @@ from hardware import Paperang
 
 printer = Paperang('FC:58:FA:32:69:2D')
 if printer.connected:
-    self.printer_hardware.sendImageToBt('test.jpg')
+    printer.sendImageToBt('test.jpg')
